092bf39a787114c2430c8c63c3b38ffe484bf29e
lincoln <linc.miller@gmail.com>
2019-07-21 00:19:24 -0400

    completed lesson 4, moving on to lesson 5

--- app/src/main/java/com/ltmiller/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/ltmiller/swoosh/WelcomeActivity.kt
@@ -1,12 +1,21 @@
 package com.ltmiller.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // use explicit intent to switch actions we KNOW we want to go to
+        // as opposed to implicit intents, which go to apps that have the same type of action
+        getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
