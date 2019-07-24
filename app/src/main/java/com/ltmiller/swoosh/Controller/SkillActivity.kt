fda697a59a06d64e415ac689f5bb7de91cd4400c
lincoln <linc.miller@gmail.com>
2019-07-23 22:32:27 -0400

    learned how to save the instance of the parcelable to retrieve data in between screen rotations as well as added a new launch icon, final lesson.

--- app/src/main/java/com/ltmiller/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/ltmiller/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.ltmiller.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.ltmiller.swoosh.Model.Player
@@ -14,11 +15,23 @@ class SkillActivity : BaseActivity() {
 
     lateinit var player : Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
 
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onBeginnerClicked(view: View){
